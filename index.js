46a4c6716031bf332810b12e18590a4152622624
mahfuz47 <mahfuzshikder560@gmail.com>
2022-05-04 18:35:46 +0600

    solving server side problems

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
-const port = process.env.PORT || 5000;
 const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3001;
 
 // middleware
 app.use(cors());
